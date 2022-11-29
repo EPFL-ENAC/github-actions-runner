5c6780af463aaf1611005dd8fb832b9db26368db
David Tang <david.tang@epfl.ch>
2022-11-29 15:56:27 +0100

    feat(cd): add runner labels

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 # https://testdriven.io/blog/github-actions-docker/
 
 # https://docs.github.com/en/rest/actions/self-hosted-runners#create-a-registration-token-for-an-organization
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REGISTRATION_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REPOSITORY_URL=https://github.com/${ORGANIZATION}
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+./config.sh --url ${REPOSITORY_URL} --token ${REGISTRATION_TOKEN} --labels ${LABELS}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token ${REGISTRATION_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
